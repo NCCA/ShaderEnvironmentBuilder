5d8a51bb34b563ed7e5417467e64e50d0f71eeac
Ting AdamJonathan <i7247470@w10124.student.bournemouth.ac.uk>
2015-11-09 19:03:41 +0000

    Manually works for each line division

--- Parsing/parsing.cpp
+++ Parsing/parsing.cpp
@@ -8,32 +8,40 @@
 int main()
 {
 	
-	std::ofstream m_fileIn;
+	std::ifstream m_fileIn;
 	m_fileIn.open("parsingInput.txt");
 	if(!m_fileIn.is_open())    ///If it can be opened
 	{
-		std::cerr<<"couldn't' open file\n";
+		std::cerr<<"File Does Not Exist (make sure that the file is readable) \n";
 		exit(EXIT_FAILURE);
 	}
 	
     std::string m_importString = "I musT sepArate This intO sectionS";
     std::vector<std::string> m_import;
-	std::istringstream divideIt(m_importString);
+	std::istringstream divideIt(fileIn);
 
+	std::string a; //manually create for testing
+	std::string b;
+	std::string c;
+	m_fileIn>>a;
+	m_fileIn>>b;
+	m_fileIn>>c;
+    a.c_str(); //works manually
+    b.c_str();
+    m_importString.c_str();
+	std::cout<<a<<std::endl;
+	std::cout<<b<<std::endl;
 
-    std::string m_import1 = "";
-    std::string m_import2 = "";
-    std::string m_import3 = "";
-
-
-
-	for(std::string m_token; getline(divideIt, m_token, '\n'); )
+	for(std::string m_token; getline(divideIt, m_token, ' '); )
 	{
-		m_fileIn>>m_import.push_back(m_token);
+		m_fileIn>>m_import.push_back(m_token); //doesnt work in a for loop... 
 	}
+	std::copy(m_import.begin(), m_import.end(), std::ostream_iterator<std::string>(std::cout, "."));
+    //m_fileIn.c_str();
+
     m_fileIn.close();
-    
     /*
+    
 	//http://rosettacode.org/wiki/Tokenize_a_string    <- Adjusted this link Starts
     std::string m_file = "I musT sepArate This intO sectionS";
     std::string m_file2 = "DIVIDE intO sectionS";
