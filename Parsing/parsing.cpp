ec708125fe60bdf5728fbb958edf5689543c5995
Ting AdamJonathan <i7247470@w10124.student.bournemouth.ac.uk>
2015-11-09 18:37:20 +0000

    Broke it trying to read import files

--- Parsing/parsing.cpp
+++ Parsing/parsing.cpp
@@ -8,42 +8,53 @@
 int main()
 {
 	
-	std::ofstream m_fileOut;
-	m_fileOut.open("ParsingInput.txt");
-	if(!m_fileOut.is_open())    ///If it can be opened
+	std::ofstream m_fileIn;
+	m_fileIn.open("parsingInput.txt");
+	if(!m_fileIn.is_open())    ///If it can be opened
 	{
 		std::cerr<<"couldn't' open file\n";
 		exit(EXIT_FAILURE);
 	}
 	
-
-	std::vector<std::string> m_import;
-	
-	//m_fileOut<<
-
+    std::string m_importString = "I musT sepArate This intO sectionS";
+    std::vector<std::string> m_import;
+	std::istringstream divideIt(m_importString);
 
 
+    std::string m_import1 = "";
+    std::string m_import2 = "";
+    std::string m_import3 = "";
+
+
+
+	for(std::string m_token; getline(divideIt, m_token, '\n'); )
+	{
+		m_fileIn>>m_import.push_back(m_token);
+	}
+    m_fileIn.close();
+    
+    /*
 	//http://rosettacode.org/wiki/Tokenize_a_string    <- Adjusted this link Starts
     std::string m_file = "I musT sepArate This intO sectionS";
     std::string m_file2 = "DIVIDE intO sectionS";
     std::vector<std::string> m_vectorList;
     std::istringstream divide(m_file2);
     std::istringstream divide1(m_file);
-    for(std::string m_token; getline(divide1, m_token, ' '); ) //editted spacing, what gets replaced? ' '
-        m_vectorList.push_back(m_token);
+    for(std::string m_token; getline(divide1, m_token, '\n'); ) //editted spacing, what gets replaced? ' '
+		{m_vectorList.push_back(m_token);}
     std::copy(m_vectorList.begin(), m_vectorList.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
 	//http://rosettacode.org/wiki/Tokenize_a_string    <- Adjusted this link ends
 
     //printf(s);
     m_file.c_str();
     
+*/
     
     
     
     
     
     
-    
-    m_fileOut.close();
+
     
 }
