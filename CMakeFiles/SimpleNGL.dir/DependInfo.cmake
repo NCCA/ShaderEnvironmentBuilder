92f2acce1968d81a5119db1542e0aff3ac0971f9
Rouse Philip <i7219595@w10154.student.bournemouth.ac.uk>
2015-11-16 17:46:08 +0000

    Added initial project + qscintilla libs and includes

---
+++ CMakeFiles/SimpleNGL.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/i7219595/SimpleNGL/SimpleNGL_automoc.cpp" "/home/i7219595/SimpleNGL/CMakeFiles/SimpleNGL.dir/SimpleNGL_automoc.cpp.o"
+  "/home/i7219595/SimpleNGL/src/NGLScene.cpp" "/home/i7219595/SimpleNGL/CMakeFiles/SimpleNGL.dir/src/NGLScene.cpp.o"
+  "/home/i7219595/SimpleNGL/src/main.cpp" "/home/i7219595/SimpleNGL/CMakeFiles/SimpleNGL.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ADDLARGEMODELS"
+  "LINUX"
+  "QT5BUILD"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_WIDGETS_LIB"
+  "_FILE_OFFSET_BITS=64"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "include"
+  "/home/i7219595/NGL/include"
+  "/opt/qt/5.5/gcc_64/include"
+  "/opt/qt/5.5/gcc_64/include/QtOpenGL"
+  "/opt/qt/5.5/gcc_64/include/QtWidgets"
+  "/opt/qt/5.5/gcc_64/include/QtGui"
+  "/opt/qt/5.5/gcc_64/include/QtCore"
+  "/opt/qt/5.5/gcc_64/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
