a337039460ea357e6af763be03cf6882258b6239
Ting AdamJonathan <i7247470@w10118.student.bournemouth.ac.uk>
2015-11-16 18:18:21 +0000

    notes on uniforms and restarting parsing

--- Parsing/parsing.cpp
+++ Parsing/parsing.cpp
@@ -7,7 +7,7 @@
 #include <fstream>
 int main()
 {
-	
+	//IMPORT FILE if it doesnt work. Delete.
 	std::ifstream m_fileIn;
 	m_fileIn.open("parsingInput.txt");
 	if(!m_fileIn.is_open())    ///If it can be opened
@@ -15,26 +15,37 @@ int main()
 		std::cerr<<"File Does Not Exist (make sure that the file is readable) \n";
 		exit(EXIT_FAILURE);
 	}
-	
-    std::string m_importString = "I musT sepArate This intO sectionS";
-    std::vector<std::string> m_import;
-	std::istringstream divideIt(fileIn);
+	//Separating EXTERNAL String individually
 
 	std::string a; //manually create for testing
 	std::string b;
 	std::string c;
+	std::string d;
+	std::string e;
 	m_fileIn>>a;
 	m_fileIn>>b;
 	m_fileIn>>c;
+	m_fileIn>>d;
+	m_fileIn>>e;
     a.c_str(); //works manually
     b.c_str();
-    m_importString.c_str();
+    c.c_str();
+    d.c_str();
+    e.c_str();
 	std::cout<<a<<std::endl;
 	std::cout<<b<<std::endl;
-
-	for(std::string m_token; getline(divideIt, m_token, ' '); )
+	std::cout<<c<<std::endl;
+	std::cout<<d<<std::endl;
+	//std::cout<<e<<std::endl;
+	
+	//Separating Internal String
+    std::string m_importString = "I musT sepArate This intO sectionS";
+    std::vector<std::string> m_import;
+	std::istringstream divideIt(m_importString);
+    m_importString.c_str();
+	for(std::string m_token; getline(divideIt, m_token, ' ');) 
 	{
-		m_fileIn>>m_import.push_back(m_token); //doesnt work in a for loop... 
+		m_import.push_back(m_token); //doesnt work in a for loop... 
 	}
 	std::copy(m_import.begin(), m_import.end(), std::ostream_iterator<std::string>(std::cout, "."));
     //m_fileIn.c_str();
