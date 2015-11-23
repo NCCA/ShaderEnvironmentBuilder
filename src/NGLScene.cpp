3ca9142b14b493cad9ac81e23bf6406d2d6e5f0d
JFDesigner <jonflynndesigner@gmail.com>
2015-11-23 17:57:18 +0000

    interface and ui setup. BASE PROJECT COMMIT

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -1,65 +1,50 @@
-#include <QMouseEvent>
-#include <QGuiApplication>
-
 #include "NGLScene.h"
-#include <ngl/Camera.h>
+#include <iostream>
+#include <ngl/Vec3.h>
 #include <ngl/Light.h>
-#include <ngl/Material.h>
 #include <ngl/NGLInit.h>
 #include <ngl/VAOPrimitives.h>
+#include <ngl/Material.h>
 #include <ngl/ShaderLib.h>
+#include <QColorDialog>
+
 
 //----------------------------------------------------------------------------------------------------------------------
-/// @brief the increment for x/y translation with mouse movement
+NGLScene::NGLScene( QWidget *_parent ) : QOpenGLWidget( _parent )
+{
+
+  // set this widget to have the initial keyboard focus
+  setFocus();
+  // re-size the widget to that of the parent (in this case the GLFrame passed in on construction)
+  this->resize(_parent->size());
+	m_wireframe=false;
+	m_rotation=0.0;
+	m_scale=1.0;
+	m_position=0.0;
+
+	m_selectedObject=0;
+}
+
+// This virtual function is called once before the first call to paintGL() or resizeGL(),
+//and then once whenever the widget has been assigned a new QGLContext.
+// This function should set up any required OpenGL context rendering flags, defining display lists, etc.
+
 //----------------------------------------------------------------------------------------------------------------------
-const static float INCREMENT=0.01f;
-//----------------------------------------------------------------------------------------------------------------------
-/// @brief the increment for the wheel zoom
-//----------------------------------------------------------------------------------------------------------------------
-const static float ZOOM=0.1f;
-
-NGLScene::NGLScene()
-{
-  // re-size the widget to that of the parent (in that case the GLFrame passed in on construction)
-  m_rotate=false;
-  // mouse rotation values set to 0
-  m_spinXFace=0.0f;
-  m_spinYFace=0.0f;
-  setTitle("Qt5 Simple NGL Demo");
-}
-
-
-NGLScene::~NGLScene()
-{
-  std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
-}
-
-void NGLScene::resizeGL(QResizeEvent *_event)
-{
-  m_width=_event->size().width()*devicePixelRatio();
-  m_height=_event->size().height()*devicePixelRatio();
-  // now set the camera size values as the screen size has changed
-  m_cam.setShape(45.0f,(float)width()/height(),0.05f,350.0f);
-}
-
-void NGLScene::resizeGL(int _w , int _h)
-{
-  m_cam.setShape(45.0f,(float)_w/_h,0.05f,350.0f);
-  m_width=_w*devicePixelRatio();
-  m_height=_h*devicePixelRatio();
-}
-
 void NGLScene::initializeGL()
 {
-  // we must call that first before any other GL commands to load and link the
-  // gl commands from the lib, if that is not done program will crash
+
   ngl::NGLInit::instance();
   glClearColor(0.4f, 0.4f, 0.4f, 1.0f);			   // Grey Background
   // enable depth testing for drawing
   glEnable(GL_DEPTH_TEST);
-  // enable multisampling for smoother drawing
-  glEnable(GL_MULTISAMPLE);
-   // now to load the shader and set the values
+  /// create our camera
+  ngl::Vec3 eye(2,2,2);
+  ngl::Vec3 look(0,0,0);
+  ngl::Vec3 up(0,1,0);
+
+  m_cam.set(eye,look,up);
+  m_cam.setShape(45,float(1024/720),0.1,300);
+  // now to load the shader and set the values
   // grab an instance of shader manager
   ngl::ShaderLib *shader=ngl::ShaderLib::instance();
   // we are creating a shader called Phong
@@ -84,51 +69,64 @@ void NGLScene::initializeGL()
   // attribute 2 are the normals x,y,z
   shader->bindAttribute("Phong",2,"inNormal");
 
-  // now we have associated that data we can link the shader
+  // now we have associated this data we can link the shader
   shader->linkProgramObject("Phong");
   // and make it active ready to load values
   (*shader)["Phong"]->use();
+  shader->setShaderParam1i("Normalize",1);
+  shader->setShaderParam3f("viewerPos",m_cam.getEye().m_x,m_cam.getEye().m_y,m_cam.getEye().m_z);
+  // now pass the modelView and projection values to the shader
   // the shader will use the currently active material and light0 so set them
-  ngl::Material m(ngl::STDMAT::GOLD);
-  // load our material values to the shader into the structure material (see Vertex shader)
+  ngl::Material m(ngl::STDMAT::POLISHEDSILVER);
   m.loadToShader("material");
-  // Now we will create a basic Camera from the graphics library
-  // This is a static camera so it only needs to be set once
-  // First create Values for the camera position
-  ngl::Vec3 from(0,1,1);
-  ngl::Vec3 to(0,0,0);
-  ngl::Vec3 up(0,1,0);
-  // now load to our new camera
-  m_cam.set(from,to,up);
-  // set the shape using FOV 45 Aspect Ratio based on Width and Height
-  // The final two are near and far clipping planes of 0.5 and 10
-  m_cam.setShape(45.0f,(float)720.0/576.0f,0.05f,350.0f);
-  shader->setUniform("viewerPos",m_cam.getEye().toVec3());
-  // now create our light that is done after the camera so we can pass the
+  // we need to set a base colour as the material isn't being used for all the params
+  shader->setShaderParam4f("Colour",0.23125f,0.23125f,0.23125f,1);
+
+  ngl::Light light(ngl::Vec3(2,2,2),ngl::Colour(1,1,1,1),ngl::Colour(1,1,1,1),ngl::LightModes::POINTLIGHT);
+  // now create our light this is done after the camera so we can pass the
   // transpose of the projection matrix to the light to do correct eye space
   // transformations
   ngl::Mat4 iv=m_cam.getViewMatrix();
   iv.transpose();
-  ngl::Light light(ngl::Vec3(-2,5,2),ngl::Colour(1,1,1,1),ngl::Colour(1,1,1,1),ngl::LightModes::POINTLIGHT );
   light.setTransform(iv);
+  light.setAttenuation(1,0,0);
+  light.enable();
   // load these values to the shader as well
   light.loadToShader("light");
-  // as re-size is not explicitly called we need to do that.
-  // set the viewport for openGL we need to take into account retina display
+
+  ngl::VAOPrimitives *prim=ngl::VAOPrimitives::instance();
+  prim->createSphere("sphere",1.0,40);
+  m_text.reset(  new  ngl::Text(QFont("Arial",18)));
+  m_text->setScreenSize(this->size().width(),this->size().height());
+  m_text->setColour(1.0,1.0,0.0);
+  update();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+//This virtual function is called whenever the widget has been resized.
+// The new size is passed in width and height.
+void NGLScene::resizeGL(QResizeEvent *_event )
+{
+  m_width=_event->size().width()*devicePixelRatio();
+  m_height=_event->size().height()*devicePixelRatio();
+  // now set the camera size values as the screen size has changed
+  m_cam.setShape(45.0f,(float)width()/height(),0.05f,350.0f);
+
 }
 
 
 void NGLScene::loadMatricesToShader()
 {
   ngl::ShaderLib *shader=ngl::ShaderLib::instance();
+  (*shader)["Phong"]->use();
 
   ngl::Mat4 MV;
   ngl::Mat4 MVP;
   ngl::Mat3 normalMatrix;
   ngl::Mat4 M;
-  M=m_mouseGlobalTX;
-  MV=  M*m_cam.getViewMatrix();
-  MVP= M*m_cam.getVPMatrix();
+  M=m_transform.getMatrix();
+  MV=m_transform.getMatrix()*m_cam.getViewMatrix();
+  MVP=MV*m_cam.getProjectionMatrix() ;
   normalMatrix=MV;
   normalMatrix.inverse();
   shader->setShaderParamFromMat4("MV",MV);
@@ -137,143 +135,139 @@ void NGLScene::loadMatricesToShader()
   shader->setShaderParamFromMat4("M",M);
 }
 
+//----------------------------------------------------------------------------------------------------------------------
+//This virtual function is called whenever the widget needs to be painted.
+// this is our main drawing routine
 void NGLScene::paintGL()
 {
-  glViewport(0,0,m_width,m_height);
-  // clear the screen and depth buffer
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  // grab an instance of the shader manager
+  glViewport(0,0,m_width,m_height);
+  ngl::VAOPrimitives *prim=ngl::VAOPrimitives::instance();
+  if(m_wireframe == true)
+  {
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+  }
+  else
+  {
+    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+  }
   ngl::ShaderLib *shader=ngl::ShaderLib::instance();
   (*shader)["Phong"]->use();
 
-  // Rotation based on the mouse position for our global transform
-  ngl::Mat4 rotX;
-  ngl::Mat4 rotY;
-  // create the rotation matrices
-  rotX.rotateX(m_spinXFace);
-  rotY.rotateY(m_spinYFace);
-  // multiply the rotations
-  m_mouseGlobalTX=rotY*rotX;
-  // add the translations
-  m_mouseGlobalTX.m_m[3][0] = m_modelPos.m_x;
-  m_mouseGlobalTX.m_m[3][1] = m_modelPos.m_y;
-  m_mouseGlobalTX.m_m[3][2] = m_modelPos.m_z;
-
-   // get the VBO instance and draw the built in teapot
-  ngl::VAOPrimitives *prim=ngl::VAOPrimitives::instance();
-  // draw
-  loadMatricesToShader();
-  prim->draw("teapot");
-
-
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mouseMoveEvent (QMouseEvent * _event)
-{
-  // note the method buttons() is the button state when event was called
-  // that is different from button() which is used to check which button was
-  // pressed when the mousePress/Release event is generated
-  if(m_rotate && _event->buttons() == Qt::LeftButton)
-  {
-    int diffx=_event->x()-m_origX;
-    int diffy=_event->y()-m_origY;
-    m_spinXFace += (float) 0.5f * diffy;
-    m_spinYFace += (float) 0.5f * diffx;
-    m_origX = _event->x();
-    m_origY = _event->y();
-    update();
-
-  }
-        // right mouse translate code
-  else if(m_translate && _event->buttons() == Qt::RightButton)
-  {
-    int diffX = (int)(_event->x() - m_origXPos);
-    int diffY = (int)(_event->y() - m_origYPos);
-    m_origXPos=_event->x();
-    m_origYPos=_event->y();
-    m_modelPos.m_x += INCREMENT * diffX;
-    m_modelPos.m_y -= INCREMENT * diffY;
-    update();
-
-   }
-}
-
-
-//----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mousePressEvent ( QMouseEvent * _event)
-{
-  // that method is called when the mouse button is pressed in this case we
-  // store the value where the maouse was clicked (x,y) and set the Rotate flag to true
-  if(_event->button() == Qt::LeftButton)
-  {
-    m_origX = _event->x();
-    m_origY = _event->y();
-    m_rotate =true;
-  }
-  // right mouse translate mode
-  else if(_event->button() == Qt::RightButton)
-  {
-    m_origXPos = _event->x();
-    m_origYPos = _event->y();
-    m_translate=true;
-  }
-
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mouseReleaseEvent ( QMouseEvent * _event )
-{
-  // that event is called when the mouse button is released
-  // we then set Rotate to false
-  if (_event->button() == Qt::LeftButton)
-  {
-    m_rotate=false;
-  }
-        // right mouse translate mode
-  if (_event->button() == Qt::RightButton)
-  {
-    m_translate=false;
-  }
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-void NGLScene::wheelEvent(QWheelEvent *_event)
-{
-
-	// check the diff of the wheel position (0 means no change)
-	if(_event->delta() > 0)
+	m_transform.setPosition(m_position);
+	m_transform.setScale(m_scale);
+	m_transform.setRotation(m_rotation);
+	loadMatricesToShader();
+	switch(m_selectedObject)
 	{
-		m_modelPos.m_z+=ZOOM;
+		case 0 : prim->draw("teapot"); break;
+		case 1 : prim->draw("sphere"); break;
+		case 2 : prim->draw("cube"); break;
 	}
-	else if(_event->delta() <0 )
-	{
-		m_modelPos.m_z-=ZOOM;
-	}
+	m_text->renderText(10,10,"Qt Gui Demo");
+}
+
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+void NGLScene::mouseMoveEvent ( QMouseEvent * _event )
+{
+  Q_UNUSED(_event);
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+void NGLScene::mousePressEvent (QMouseEvent * _event  )
+{
+  Q_UNUSED(_event);
+
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+void NGLScene::mouseReleaseEvent (  QMouseEvent * _event  )
+{
+  Q_UNUSED(_event);
+
+}
+
+
+
+NGLScene::~NGLScene()
+{
+}
+
+void NGLScene::toggleWireframe(bool _mode	 )
+{
+	m_wireframe=_mode;
 	update();
 }
-//----------------------------------------------------------------------------------------------------------------------
 
-void NGLScene::keyPressEvent(QKeyEvent *_event)
+void NGLScene::setXRotation( double _x		)
 {
-  // that method is called every time the main window recives a key event.
-  // we then switch on the key value and set the camera in the GLWindow
-  switch (_event->key())
-  {
-  // escape key to quit
-  case Qt::Key_Escape : QGuiApplication::exit(EXIT_SUCCESS); break;
-  // turn on wirframe rendering
-  case Qt::Key_W : glPolygonMode(GL_FRONT_AND_BACK,GL_LINE); break;
-  // turn off wire frame
-  case Qt::Key_S : glPolygonMode(GL_FRONT_AND_BACK,GL_FILL); break;
-  // show full screen
-  case Qt::Key_F : showFullScreen(); break;
-  // show windowed
-  case Qt::Key_N : showNormal(); break;
-  default : break;
-  }
-  // finally update the GLWindow and re-draw
-  //if (isExposed())
+	m_rotation.m_x=_x;
+	update();
+}
+
+void NGLScene::setYRotation( double _y	)
+{
+	m_rotation.m_y=_y;
+	update();
+}
+void NGLScene::setZRotation( double _z )
+{
+	m_rotation.m_z=_z;
+	update();
+}
+
+void NGLScene::setXScale( double _x	)
+{
+	m_scale.m_x=_x;
+	update();
+}
+
+void NGLScene::setYScale(	 double _y)
+{
+	m_scale.m_y=_y;
+	update();
+}
+void NGLScene::setZScale( double _z )
+{
+	m_scale.m_z=_z;
+	update();
+}
+
+void NGLScene::setXPosition( double _x	)
+{
+	m_position.m_x=_x;
+	update();
+}
+
+void NGLScene::setYPosition( double _y	)
+{
+	m_position.m_y=_y;
+	update();
+}
+void NGLScene::setZPosition( double _z	 )
+{
+	m_position.m_z=_z;
+	update();
+}
+
+void NGLScene::setObjectMode(	int _i)
+{
+	m_selectedObject=_i;
+	update();
+}
+void NGLScene::setColour()
+{
+	QColor colour = QColorDialog::getColor();
+	if( colour.isValid())
+	{
+    ngl::ShaderLib *shader=ngl::ShaderLib::instance();
+    (*shader)["Phong"]->use();
+    shader->setShaderParam4f("Colour",colour.redF(),colour.greenF(),colour.blueF(),1.0);
     update();
+	}
 }
