9f9f933fb953e0139ad0210f6f31d677ae01f986
jlyddon <lyddon60@hotmail.com>
2016-02-11 09:35:45 +0000

    Button creation updated

--- buttonCreation/src/main.cpp
+++ buttonCreation/src/main.cpp
@@ -11,8 +11,17 @@ int main(int argc, char *argv[])
 {
   //readFile();
   QApplication app(argc, argv);
-  Button myButton;
-  myButton.show();
+
+  QGridLayout *mainLayout_2;
+  mainLayout_2 = new QGridLayout;/*
+  mainLayout->addWidget(buttonBox, 0, 1);
+  setLayout(mainLayout);
+
+  mainLayout->setSizeConstraint(QLayout::SetMinimumSize);
+
+  setWindowTitle(tr("Button Layout"));*/
+  //Button myButton;
+  //myButton.show();
 
   return app.exec();
 }
