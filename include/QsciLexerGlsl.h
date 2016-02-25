1da77d6e262ae7e49b53d1a98c976b38bc74bf5b
Phil Rouse <phil@philrou.se>
2016-02-25 10:45:42 +0000

    Commented and formatted to NCCA standards

--- include/QsciLexerGlsl.h
+++ include/QsciLexerGlsl.h
@@ -5,42 +5,101 @@
 #include <Qsci/qscistyle.h>
 #include <Qsci/qsciapis.h>
 
-class QsciLexerGLSL: public QsciLexerCustom {
+//----------------------------------------------------------------------------------------------------------------------
+/// \file QsciLexelGlsl.h
+/// \brief Custom QScintilla lexer for GLSL
+/// \author Phil Rouse
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+/// \brief Custom QScintilla lexer for GLSL
+//----------------------------------------------------------------------------------------------------------------------
+class QsciLexerGLSL: public QsciLexerCustom
+{
 public:
-    QsciLexerGLSL(QsciScintilla *parent=0);
-    ~QsciLexerGLSL();
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief enum for supported style types
+  //----------------------------------------------------------------------------------------------------------------------
+  enum StyleType
+  {
+    NONE,
+    DEFAULT,
+    NUMBER,
+    KEYWORD,
+    FUNCTION,
+    STRING,
+    COMMENT,
+    DISABLED,
+    OPERATOR,
+    WHITESPACE,
+    DATATYPE
+  };
 
-    void styleText(int start, int end);
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief QsciLexerGLSL constructor
+  /// \param [in] _parent Parent QsciScintilla instance
+  //----------------------------------------------------------------------------------------------------------------------
+  QsciLexerGLSL(QsciScintilla *_parent=0);
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief QsciLexerGLSL destructor.
+  //----------------------------------------------------------------------------------------------------------------------
+  virtual ~QsciLexerGLSL();
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief virtual function reimplemented from qscilexercustom.
+  /// This is called when text needs to be styled.
+  /// \param [in] _start  Start position of text to be styled.
+  /// \param [in] _end    End position of text to be styleed.
+  //----------------------------------------------------------------------------------------------------------------------
+  virtual void styleText(int _start, int _end);
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief virtual function reimplemented from qscilexer.
+  /// \return lexer language.
+  //----------------------------------------------------------------------------------------------------------------------
+  virtual const char* language() const;
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief virtual function reimplemented from qscilexer.
+  /// \param [in] _style  style number.
+  /// \return Default colour for style \a _style.
+  //----------------------------------------------------------------------------------------------------------------------
+  virtual QColor defaultColor(int _style) const;
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief virtual function reimplemented from qscilexer.
+  /// \param [in] _style  style number.
+  /// \return Default paper colour for style \a _style.
+  //----------------------------------------------------------------------------------------------------------------------
+  virtual QColor defaultPaper(int _style) const;
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief virtual function reimplemented from qscilexer.
+  /// \param [in] _style  style number.
+  /// \return Default font for style \a _style.
+  //----------------------------------------------------------------------------------------------------------------------
+  virtual QFont defaultFont(int _style) const;
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief virtual function reimplemented from qscilexer.
+  /// \param [in] _style  style number.
+  /// \return Descriptive for style \a _style.
+  //----------------------------------------------------------------------------------------------------------------------
+  virtual QString description(int _style) const;
 
-    const char *language() const;
-
-    QColor defaultColor(int style) const;
-    QColor defaultPaper(int style) const;
-    QFont defaultFont(int style) const;
-
-    QString description(int style) const;
 
 
-    enum StyleType{
-        NONE,
-        DEFAULT,
-        NUMBER,
-        KEYWORD,
-        FUNCTION,
-        STRING,
-        COMMENT,
-        DISABLED,
-        OPERATOR,
-        WHITESPACE,
-        DATATYPE
-    };
-
-private:
-    QsciScintilla *m_parent;
-    QsciAPIs *m_API;
-    QsciLexerGLSL(const QsciLexerGLSL &);
-    QsciLexerGLSL &operator=(const QsciLexerGLSL &);
-    QsciStyle m_keywordStyle;
+protected:
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief parent QScintilla instance
+  //----------------------------------------------------------------------------------------------------------------------
+  QsciScintilla   *m_parent;
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief glsl API instance
+  //----------------------------------------------------------------------------------------------------------------------
+  QsciAPIs        *m_API;
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief copy constructor
+  //----------------------------------------------------------------------------------------------------------------------
+  QsciLexerGLSL(const QsciLexerGLSL &);
+  //----------------------------------------------------------------------------------------------------------------------
+  /// \brief assignment operator
+  //----------------------------------------------------------------------------------------------------------------------
+  QsciLexerGLSL &operator=(const QsciLexerGLSL &);
 };
 
 #endif
